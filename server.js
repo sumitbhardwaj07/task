a6646b5d8dd670404a2aee9071eaa71fa5f5dfad
sumitbhardwaj07 <150799672+sumitbhardwaj07@users.noreply.github.com>
2025-06-05 23:26:04 +0530

    more changes

--- server.js
+++ server.js
@@ -2,19 +2,43 @@ const app = require('./app');
 const db = require('./src/models');
 require('dotenv').config();
 
+let dbInitialized = false;
 
-const PORT = process.env.PORT || 3001;
-
-
-(async () => {
-  try {
-    await db.sequelize.sync(); // use { force: true } for dev reset
-    console.log('Database connected');
-
-    app.listen(PORT, () => {
-      console.log(`Server running on http://localhost:${PORT}`);
-    });
-  } catch (err) {
-    console.error('Failed to start server:', err);
+app.use(async (req, res, next) => {
+  if (!dbInitialized) {
+    try {
+      await db.sequelize.sync(); 
+      console.log('Database connected');
+      dbInitialized = true;
+    } catch (err) {
+      console.error('Database connection failed:', err);
+      return res.status(500).send('Database connection error');
+    }
   }
-})();
+  next();
+});
+
+module.exports = app;
+
+
+
+// const app = require('./app');
+// const db = require('./src/models');
+// require('dotenv').config();
+
+
+// const PORT = process.env.PORT || 3001;
+
+
+// (async () => {
+//   try {
+//     await db.sequelize.sync(); // use { force: true } for dev reset
+//     console.log('Database connected');
+
+//     app.listen(PORT, () => {
+//       console.log(Server running on http://localhost:${PORT});
+//     });
+//   } catch (err) {
+//     console.error('Failed to start server:', err);
+//   }
+// })();
